8d5b40de67286e5f88c2a450b640ca1411f54623
Brita Ramsay <britaramsay@gmail.com>
2018-05-24 21:37:58 -0400

    Added MVC folder structure

---
+++ config/orm.js
@@ -0,0 +1,6 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
---
+++ controllers/controller.js
@@ -0,0 +1,6 @@
+const express = require("express"),
+      router = express.Router();
+
+//Import models
+
+module.exports = router;
