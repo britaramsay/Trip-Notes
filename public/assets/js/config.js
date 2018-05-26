44c3b4589fa25133212096d05033cb74d914372d
Marcela Errazquin <merrazquin@gmail.com>
2018-05-26 09:03:29 -0700

    Moved Firebase env vars into .env file, committing config.js

---
+++ public/assets/js/config.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+
+var config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+};
+firebase.initializeApp(config);
+
+function onSignIn(googleUser) {
+    console.log('Google Auth Response', googleUser);
+
+    // Google idtoken
+    var id_token = googleUser.getAuthResponse().id_token
+
+    // Build Firebase credential with the Google ID token.
+    var credential = firebase.auth.GoogleAuthProvider.credential(id_token);
+    
+    // Firebase idtoken
+    var idToken = credential.idToken
+
+    // Sign in with credential from the Google user.
+    firebase.auth().signInAndRetrieveDataWithCredential(credential).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+}
+
+function signOut() {
+    // Signout with firebase
+    firebase.auth().signOut().then(function() {
+        var auth2 = gapi.auth2.getAuthInstance();
+
+        // Have to sign out with google too or user will be signed in on reload
+        auth2.signOut().then(function () {
+            console.log('User signed out.');
+        });
+        // Sign-out successful.
+      }).catch(function(error) {
+        // An error happened.
+    });
+}
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // Show signout button when signed in
+        $('.g-signout2').show()
+
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+
+    } else {
+        // User is signed out. Hide signout button
+        $('.g-signout2').hide()
+    }
+});
